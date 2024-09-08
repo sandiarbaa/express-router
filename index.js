afb53fe622f89f4dd9c34ceaf0f053bb62604cd6
sandiarbaa <sandiarbaa@gmail.com>
2024-09-08 21:35:02 +0700

    membuat signed cookie atau cookie yang di segel

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ const port = 3000;
 // define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); // digunakan sebagai middleware
+// tambahkan string apapun untuk menjadi kata kunci, di sini contohnya 'secret-key'
+// dia ini sebagai kata kunci untuk meng-enkripsi data dan berfungsi juga agar supaya dapat terbaca data yg di enkripsi tersebut
+app.use(cookieParser("secret-key")); // secret-key ini untuk eknripsi data cookies nya, dan juga untuk membaca data yg di enkripsi nantinya
+// ga harus secret-key bisa apa aja bebas, mau ditambahkan simbol atau key yg serumit mungkin
+
+// route cookies example
+app.get("/signingcookie", (req, res) => {
+  // sign itu seperti menyegel atau memberikan penanda bahwa datanya itu aman, dan dari sumber yg terpercata
+  // cookie nya bukan menjadikan di seperti enkripsi string acak
+  res.cookie("paket", "ransel", { signed: true }); // param 1 name, param 2 cookie value // true, supaya datanya di enkripsi
+  res.send("signing cookie");
+});
+
+// KARENA kita juga menggunakan key yg sama baik itu untuk meng-generate/mendapatkan cookienya
+app.get("/verifycookie", (req, res) => {
+  const cookies = req.signedCookies; // untuk mendapatkan cookie yg di enkripsi/sign
+  res.send(cookies);
+});
 
 // define routes
 app.use("/admin", require("./routes/admin"));
