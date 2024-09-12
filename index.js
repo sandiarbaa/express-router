401d8e6560fe8de0871d8390014d3d8102831ade
sandiarbaa <sandiarbaa@gmail.com>
2024-09-12 18:36:58 +0700

    implementasi session di express

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser"); // install lib cookie-parser dulu karena req tidak bisa langsung membaca cookies
+// lib
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 // define port
 const port = 3000;
+const secret = "secret-key";
 
 // define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// tambahkan string apapun untuk menjadi kata kunci, di sini contohnya 'secret-key'
-// dia ini sebagai kata kunci untuk meng-enkripsi data dan berfungsi juga agar supaya dapat terbaca data yg di enkripsi tersebut
-app.use(cookieParser("secret-key")); // secret-key ini untuk eknripsi data cookies nya, dan juga untuk membaca data yg di enkripsi nantinya
-// ga harus secret-key bisa apa aja bebas, mau ditambahkan simbol atau key yg serumit mungkin
+app.use(cookieParser(secret));
+app.use(
+  // setup
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // route cookies example
 app.get("/signingcookie", (req, res) => {
-  // sign itu seperti menyegel atau memberikan penanda bahwa datanya itu aman, dan dari sumber yg terpercata
-  // cookie nya bukan menjadikan di seperti enkripsi string acak
-  res.cookie("paket", "ransel", { signed: true }); // param 1 name, param 2 cookie value // true, supaya datanya di enkripsi
+  res.cookie("paket", "ransel", { signed: true });
   res.send("signing cookie");
 });
 
-// KARENA kita juga menggunakan key yg sama baik itu untuk meng-generate/mendapatkan cookienya
 app.get("/verifycookie", (req, res) => {
-  const cookies = req.signedCookies; // untuk mendapatkan cookie yg di enkripsi/sign
+  const cookies = req.signedCookies;
   res.send(cookies);
+});
+
+app.get("/count", (req, res) => {
+  // kalau ada session
+  if (req.session.count) {
+    // membuat variabel count gitu sih, isinya bebas, namanya juga ga harus count
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`count:${req.session.count}`);
 });
 
 // define routes
